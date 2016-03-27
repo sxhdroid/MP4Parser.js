47afabc28382e6751ebb726ddadf5403ddceb053
uupaa <uupaa.js@gmail.com>
2016-03-28 03:13:26 +0900

    add diagnosticInformationObject to MP4BoxTreeObject

--- test/testcase.js
+++ test/testcase.js
@@ -35,10 +35,48 @@ function testMP4Parse_parseMP4File(test, pass, miss) {
     var jsonFile = "../assets/json/png.00.mp4.json";
     var task = new Task("testMP4Parse_parseMP4File", 2, function(error, buffer) {
         var json0 = JSON.stringify(buffer[0], null, 2);
-        var mp4box = MP4Parser.parse( new Uint8Array(buffer[1]) );
-        var json1 = JSON.stringify(mp4box, null, 2);
+        var mp4tree = MP4Parser.parse( new Uint8Array(buffer[1]) );
+        var json1 = JSON.stringify(mp4tree.root, null, 2);
 
-        if (json0 === json1) {
+        var diagnostic_boxes = [
+            "root/ftyp:32",
+            "root/moov:747",
+            "root/moov/mvhd:108",
+            "root/moov/trak:533",
+            "root/moov/trak:0/tkhd:92",
+            "root/moov/trak:0/edts:36",
+            "root/moov/trak:0/edts/elst:28",
+            "root/moov/trak:0/mdia:397",
+            "root/moov/trak:0/mdia/mdhd:32",
+            "root/moov/trak:0/mdia/hdlr:45",
+            "root/moov/trak:0/mdia/minf:312",
+            "root/moov/trak:0/mdia/minf/vmhd:20",
+            "root/moov/trak:0/mdia/minf/dinf:36",
+            "root/moov/trak:0/mdia/minf/dinf/dref:28",
+            "root/moov/trak:0/mdia/minf/dinf/dref/url :12",
+            "root/moov/trak:0/mdia/minf/stbl:248",
+            "root/moov/trak:0/mdia/minf/stbl/stsd:148",
+            "root/moov/trak:0/mdia/minf/stbl/stsd/avc1:132",
+            "root/moov/trak:0/mdia/minf/stbl/stsd/avc1/avcC:46",
+            "root/moov/trak:0/mdia/minf/stbl/stts:24",
+            "root/moov/trak:0/mdia/minf/stbl/stsc:28",
+            "root/moov/trak:0/mdia/minf/stbl/stsz:20",
+            "root/moov/trak:0/mdia/minf/stbl/stco:20",
+            "root/moov/udta:98",
+            "root/moov/udta/meta:90",
+            "root/moov/udta/meta/hdlr:33",
+            "root/moov/udta/meta/ilst:45",
+            "root/free:8",
+            "root/mdat:841",
+        ];
+
+        var ok = false;
+        if (diagnostic_boxes.join(",") === mp4tree.diagnostic.boxes.join(",")) {
+            if (json0 === json1) {
+                ok = true;
+            }
+        }
+        if (ok) {
             test.done(pass());
         } else {
             test.done(miss());
